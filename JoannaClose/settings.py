4e6386dd38e18c02fd2d650abdef6403151590c4
Jeremy Stewart <jglstewart@gmail.com>
2012-10-01 13:01:23 -0300

    added SITE_ROOT

--- JoannaClose/settings.py
+++ JoannaClose/settings.py
@@ -101,6 +101,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.messages.context_processors.messages"
 )
 
+SITE_ROOT ='' 
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
