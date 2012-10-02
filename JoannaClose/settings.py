fb4dfa689948b30f40788ff45bdcc92af695b39f
Jeremy Stewart <jglstewart@gmail.com>
2012-10-01 21:55:14 -0300

    Added context_processor for site_root

--- JoannaClose/settings.py
+++ JoannaClose/settings.py
@@ -98,10 +98,11 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
     "django.core.context_processors.static",
-    "django.contrib.messages.context_processors.messages"
+    "django.contrib.messages.context_processors.messages",
+    "blog.context_processors.site_root",
 )
 
-SITE_ROOT ='' 
+SITE_ROOT ='/' 
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
